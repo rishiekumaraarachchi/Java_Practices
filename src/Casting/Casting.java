3c29a1c06f7a7d0130881b3f164700c19c332758
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-01-02 22:27:04 +0530

    casting

---
+++ src/Casting/Casting.java
@@ -0,0 +1,10 @@
+package Casting;
+
+public class Casting {
+    public static void main(String[] args) {
+        byte b= 127;
+        int a = 12;
+        b = (byte) a; // Casting
+        System.out.println(b);
+    }
+}
